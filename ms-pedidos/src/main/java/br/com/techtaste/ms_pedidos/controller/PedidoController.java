70ebd52735103bf4bebe8f64191d9d9a84d6ace5
Willie Colatino <willie.colatino@eb.mil.br>
2025-09-25 10:03:07 -0300

    Criação do gateway e teste de loadbalancer do ms-pedidos

--- ms-pedidos/src/main/java/br/com/techtaste/ms_pedidos/controller/PedidoController.java
+++ ms-pedidos/src/main/java/br/com/techtaste/ms_pedidos/controller/PedidoController.java
@@ -4,6 +4,7 @@ import br.com.techtaste.ms_pedidos.dto.PedidoRequestDto;
 import br.com.techtaste.ms_pedidos.dto.PedidoResponseDto;
 import br.com.techtaste.ms_pedidos.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,12 @@ public class PedidoController {
     @PostMapping
     public ResponseEntity<PedidoResponseDto> cadastrarPedido(@RequestBody PedidoRequestDto pedidoDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.cadastrarPedido(pedidoDto));
+    }
+
+    @GetMapping("/response")
+    public String obterPorta(@Value("${local.server.port}") String porta) {
+        return String.format("Resposta vinda da porta %s", porta);
+
     }
 
 }
