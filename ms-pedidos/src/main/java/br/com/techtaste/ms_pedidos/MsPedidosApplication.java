9188134b883c76376265bad4e33ceb421a481564
Willie Colatino <wcolatino@hotmail.com>
2026-01-09 06:20:57 -0300

    Inserção do client para ms de pedidos

--- ms-pedidos/src/main/java/br/com/techtaste/ms_pedidos/MsPedidosApplication.java
+++ ms-pedidos/src/main/java/br/com/techtaste/ms_pedidos/MsPedidosApplication.java
@@ -2,8 +2,10 @@ package br.com.techtaste.ms_pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsPedidosApplication {
 
 	public static void main(String[] args) {
